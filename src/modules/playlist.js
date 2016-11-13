18140d883a058e0345ef35900a195504e3b0bb42
ykmikan <ykmikan821@gmail.com>
2016-11-13 16:44:03 +0900

    modify playlist add method.

--- src/modules/playlist.js
+++ src/modules/playlist.js
@@ -6,7 +6,7 @@ class Playlist {
     this.cmd = mpd.cmd
   }
 
-  add(uris) {
+  async add(uris) {
     return new Promise((resolve, reject) => {
       let args
 
@@ -20,13 +20,12 @@ class Playlist {
         reject('uri type error. uri must be array or string.')
       }
 
-      args.forEach(arg => {
-        this.mpd.sendCommand(this.cmd('add', [arg]), err => {
-          if (err) {
-            reject(err)
-          }
-        })
-      })
+      for(var i = 0, length = args.length; i < length; i++) {
+        let arg = args[i]
+
+        result = await sendCommand('add', arg)
+        console.log('result ', result)
+      }
 
       resolve()
     })
@@ -43,6 +42,18 @@ class Playlist {
       })
     })
   }
+}
+
+function sendCommand(cmd, arg) {
+  return new Promise((resolve, reject) => {
+    this.mpd.sendCommand(this.cmd(cmd, [arg]), err => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(true)
+    })
+  })
 }
 
 export default Playlist
